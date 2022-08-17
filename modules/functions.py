82ed8a1ec8d2bdbbbc7a26c865ed97d1ab7a2847
Karol Wieliczko <58177400+kwieliczko@users.noreply.github.com>
2022-08-17 19:38:38 +0200

    Update functions.py

--- modules/functions.py
+++ modules/functions.py
@@ -1,33 +1,23 @@
 from builtins import staticmethod
 
-
 class GeneralFunctions():
-    
-    
     @staticmethod
     def print_error(error_message, optional_variable=""):
         print("\n\n**********************************************")
         print("***   ", error_message, optional_variable)
         print("**********************************************\n\n")
-        
-    
-    
-    
+
     # Calculates the percentage of the current number of connections for a given snapshot in the context 
-	# of the maximum and minimum number of connections
+    # of the maximum and minimum number of connections
     @staticmethod
     def calculate_percentages(current_connections, min_connenctions, max_connenctions):
             return (current_connections - min_connenctions)/(max_connenctions-min_connenctions)*100
-    
-    
-    
-    
+
     @staticmethod
     def choose_color(current_connections, min_connenctions, max_connenctions):
         
         precentage = GeneralFunctions.calculate_percentages(current_connections, min_connenctions, max_connenctions)
-        
-		
+
         if precentage  >= 90:
             foreground_cell_color = '69bc6e'
         elif precentage <= 90 and precentage >= 80:
@@ -48,7 +38,5 @@ class GeneralFunctions():
             foreground_cell_color = 'f6fdf8'
         elif precentage <= 10 and precentage >= 0:
             foreground_cell_color = 'ffffff'
-
-
         return foreground_cell_color
         
